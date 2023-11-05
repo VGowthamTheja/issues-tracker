1cf6fd2f4761ce14393263a9a9b51f8ed289c831
VGowthamTheja <vempalligowthamtheja@gmail.com>
2023-11-06 00:39:20 +0530

    Added Navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+
+const Navbar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link, index) => {
+          return (
+            <li key={index}>
+              <Link
+                className="text-zinc-500 hover:text-zinc-800 transition-colors font-semibold"
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
